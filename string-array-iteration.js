5d0e7aa8f0aa1a14903284b410cdaca98aba573d
aniktonu <aniktonu@gmail.com>
2022-11-06 16:20:41 +0600

    string

---
+++ string-array-iteration.js
@@ -0,0 +1,21 @@
+//string iteration 
+
+let nam = "anik hasan ahmed!"
+let len = nam.length;
+
+for (i = 0; i < len; i++) {
+    console.log(`Index: ${i} `);
+    console.log(nam[i]);
+}
+
+
+
+//array iteration
+
+let arr = ["anik", "hasan", 12];
+let lan = arr.length;
+
+for (   i=0; i<lan; i++ ) {
+    console.log(`Index: ${i}`);
+console.log(arr[i]);
+}
---
+++ template-literals-ES6.js
@@ -0,0 +1,30 @@
+//Template Literals (ES6)
+// backtik sign ``
+// templete literials help how to do operation in the javascript string 
+
+let x = "hasan";
+console.log("my name is" + x);
+
+
+let name = "anik";
+let age = 35;
+let subject = "english";
+
+console.log(`his name is ${name}
+his age is ${age}
+his subject is ${subject}`)
+
+
+let a = 10;
+let b = 20;
+
+console.log(`${a} + ${b} = ${a+b}`)
+
+
+let m = 30;
+let n = 25;
+let u = 15;
+
+console.log(`my three valus are: ${m} + ${n} + ${u}`);
+
+
