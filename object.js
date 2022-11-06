e07cc3e211ff8978cfc13dca44c95108f6f2c07e
aniktonu <aniktonu@gmail.com>
2022-11-06 16:21:35 +0600

    object

---
+++ object.js
@@ -0,0 +1,46 @@
+object work with  property 
+obejct in property is like(attribute in html)
+for object we use { }
+
+
+
+
+
+
+
+
+
+var student = {
+    name: 'hello',
+    age: 25,
+    phone: '019213646',
+}
+
+
+
+// call a property of obejct in two ways 
+student.age
+student["name"]
+
+// add new property to object in two ways
+
+student.occu = "student";
+student["Meritial"] = "unmerrid";
+
+// for delete property of object 
+delete student.Meritial
+
+//emty object define
+
+var teacher = {}
+
+teacher.name = "Jitu Bhaiya"
+
+
+
+console.log(student.age)
+console.log(student["age"])
+console.log(student)
+console.log(teacher)
+console.log(teacher.name)
+
---
+++ object_method.js
@@ -0,0 +1,15 @@
+//Object method
+
+let person = {
+    firstname: "anik",
+    lastname : "hasan",
+    age: 25,
+    occupation: "Student",
+    address: "dhaka",
+        fullname: function(iteam1){
+
+            return
+        }
+
+}
+
