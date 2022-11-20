1e6a5d7777b6fd51c1e0ef4aee140725093b2ff3
aniktonu <aniktonu@gmail.com>
2022-11-20 15:36:03 +0600

    Array Destructuring

---
+++ Leveling-Up-to-ES6/array-destructuring.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "oragne"];
+let fruits1 = fruits[0];
+let fruits2 = fruits[1];
+console.log(fruits1, fruits2);
+
+
+// array distructuring
+let [fruit1,,fruit3] = fruits; // fro skip iteam just oput a emty coma (,)
+
+console.log(fruit1,fruit3);
